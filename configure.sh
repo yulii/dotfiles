e0fbdd4db6763e6633c8174d06ab5c1b6c7ca69d
Ryo Yoneyama (@yulii) <yone.info@gmail.com>
2015-08-24 17:27:20 +0900

    add .gitconfig

--- configure.sh
+++ configure.sh
@@ -9,7 +9,8 @@ trap 'echo -e "\nabort!" ; exit 1' 1 2 3 15
 
 cd $HOME
 
-ln -s $SWD/.vimrc .vimrc
-ln -s $SWD/.zshrc .zshrc
+ln -s $SWD/.gitconfig .gitconfig
+ln -s $SWD/.vimrc     .vimrc
+ln -s $SWD/.zshrc     .zshrc
 
 exit 0
