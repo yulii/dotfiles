4cbaeab6699e86d3f1716d75313f0457517dd128
Ryo Yoneyama (@yulii) <yone.info@gmail.com>
2015-09-30 07:33:34 +0900

    fixed search scope

--- .plex/function.sh
+++ .plex/function.sh
@@ -1,9 +1,9 @@
 # Repository Management {{{
 
 export PLEX_PROJECT_ROOT=$HOME
-export PLEX_PROJECT_FIND_MAX_DEPTH=6
+export PLEX_PROJECT_FIND_MAX_DEPTH=8
 function find-projects() {
-  find ${1:-$PLEX_PROJECT_ROOT} -name ".git" -type d -maxdepth $PLEX_PROJECT_FIND_MAX_DEPTH | sed -e 's/\/.git$//'
+  find ${1:-$PLEX_PROJECT_ROOT} -name ".*" -type d -maxdepth $PLEX_PROJECT_FIND_MAX_DEPTH -prune -o -name ".git" -type d -maxdepth $PLEX_PROJECT_FIND_MAX_DEPTH | grep ".git$" | sed -e 's/\/.git$//'
 }
 
 alias sp='cd "$(find-projects | peco)"'
