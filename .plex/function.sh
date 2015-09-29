8df5c9c52d483008289164494b48b84a3f8ee152
Ryo Yoneyama (@yulii) <yone.info@gmail.com>
2015-09-29 22:19:47 +0900

    fixed quote

--- .plex/function.sh
+++ .plex/function.sh
@@ -6,6 +6,6 @@ function find-projects() {
   find ${1:-$PLEX_PROJECT_ROOT} -name ".git" -type d -maxdepth $PLEX_PROJECT_FIND_MAX_DEPTH | sed -e 's/\/.git$//'
 }
 
-alias sp='cd $(find-projects | peco)'
+alias sp='cd "$(find-projects | peco)"'
 
 # }}}
