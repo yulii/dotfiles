9850d174bc604c0d1a81b3dea53c3d48fc7e0abe
Ryo Yoneyama (@yulii) <yone.info@gmail.com>
2015-11-04 18:15:28 +0900

    install atom plugins

---
+++ atom/provisioning.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -eu
+
+CWD=$(pwd)
+SWD=$(cd $(dirname $0) && pwd)
+
+# trap
+trap 'echo -e "\nabort!" ; exit 1' 1 2 3 15
+
+
+apm install file-icons
+apm install term2
+
+# Auto-Complete
+apm install autocomplete-paths
+
+
+# Git
+# apm install git-plus
+apm install git-control
+apm install git-history
